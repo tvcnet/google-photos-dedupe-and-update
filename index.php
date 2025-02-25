ca71477ce712bcc2c19978e51106ed860e065f0a
Jim Walker, The Hack Repair Guy <tvcnet@users.noreply.github.com>
2025-02-25 13:57:56 -0800

    test2

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-// Enable error reporting for debugging
+// Enable error reporting for debugging- test2
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
